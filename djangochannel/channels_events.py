6caf82a2349d06cb7a6662d57c9b22265ea54ea4
Mahammmadhusain Kadiwala <62128991+Mahammadhusain@users.noreply.github.com>
2022-06-20 11:19:46 +0530

    Update channels_events.py

--- djangochannel/channels_events.py
+++ djangochannel/channels_events.py
@@ -1,1 +1,9 @@
 
+"type":"websocket.connect" # called at when client open connection
+"type":"websocket.accept" # called for accept incoming connection (u can pass hader with this event)
+"type":"websocket.receive" # called at when server recived data(message) from client
+"type":"websocket.send" # called at when client recived data(message) from server  
+"type":"websocket.disconnect" # called at when connection lost form either client or server.   
+"type":"websocket.close" # send by the application to tell the server to close connection.   
+
+
